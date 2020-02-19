768780a0be9b6d7c8e00af9267244aafc94e45b5
cardosor2 <renato.cardoso@paddypowerbetfair.com>
2020-02-19 22:28:36 +0000

    make it pretty

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './logo512.png';
 import './App.css';
 
 function App() {
@@ -8,16 +8,8 @@ function App() {
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo"/>
                 <p>
-                    Edit <code>src/App.js</code> and save to reload.
+                    <code>Alexandre, make me that awesome UI! :)</code>
                 </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
             </header>
         </div>
     );
