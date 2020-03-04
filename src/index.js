6ee1f26c7953a20d86c5ffc89aa620bde51eb7e0
cardosor2 <renato.cardoso@paddypowerbetfair.com>
2020-03-04 18:17:33 +0000

    fixing analytics

--- src/index.js
+++ src/index.js
@@ -1,8 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import * as firebase from 'firebase/app';
+import 'firebase/analytics';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+import './index.css';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAFZFduhEa0DV1Ra1OauMUjkimbw0NzmQ4",
+    authDomain: "caixabreak.firebaseapp.com",
+    databaseURL: "https://caixabreak.firebaseio.com",
+    projectId: "caixabreak",
+    storageBucket: "caixabreak.appspot.com",
+    messagingSenderId: "708860926883",
+    appId: "1:708860926883:web:75030dc4ceae940fe55816",
+    measurementId: "G-GZ8QWTQCZK"
+};
+
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
